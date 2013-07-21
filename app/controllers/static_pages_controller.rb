82f527362dcfb4bef90fb646e6c9fa2831e019da
Ignacio Palladino <ignacio@flingo.tv>
2013-07-21 12:22:54 -0700

    removed old micropost pages, replaced with actual pages and added options for admin to create year car_model, trim, tans, eng

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,6 +2,7 @@ class StaticPagesController < ApplicationController
   def home
     if signed_in?
       @micropost  = current_user.microposts.build
+      @ferraris = Ferrari.paginate(page: params[:page])
       @feed_items = current_user.feed.paginate(page: params[:page])
     end
   end
@@ -15,3 +16,4 @@ class StaticPagesController < ApplicationController
   def contact
   end
 end
+
