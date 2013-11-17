8ba5cea57cd97ceb0d49617d731b0c69d7c969df
Ignacio Palladino <ignacio@flingo.tv>
2013-11-17 02:24:40 -0800

    precompiled

--- config/application.rb
+++ config/application.rb
@@ -52,7 +52,9 @@ module SampleApp
     # in your app. As such, your models will need to explicitly whitelist or blacklist accessible
     # parameters by using an attr_accessible or attr_protected declaration.
     config.active_record.whitelist_attributes = true
-
+    
+    config.assets.precompile += ['application-screen.css', 'application-print.css']
+    
     # Enable the asset pipeline
     config.assets.enabled = true
 
