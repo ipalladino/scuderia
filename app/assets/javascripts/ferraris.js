4b2c6e047ceb9e2ccba33e29eee27074e890a4aa
Ignacio Palladino <ignacio@flingo.tv>
2013-11-18 16:43:18 -0800

    added facebook sharing

--- app/assets/javascripts/ferraris.js
+++ app/assets/javascripts/ferraris.js
@@ -200,11 +200,28 @@ $(function() {
         },
 
         render: function(){
-          $(this.el).html("");
-          this.collection.each(function(item){
-              var html = this.template(item.toJSON());
-            $(this.el).append(html);  
-          }, this);
+            $(this.el).html("");
+            this.collection.each(function(item){
+                var html = this.template(item.toJSON());
+                $(this.el).append(html);  
+            }, this);
+          
+            $('#twitterbtn-link,#facebookbtn-link').click(function(event) {
+                var width  = 575,
+                    height = 400,
+                    left   = ($(window).width()  - width)  / 2,
+                    top    = ($(window).height() - height) / 2,
+                    url    = this.href,
+                    opts   = 'status=1' +
+                             ',width='  + width  +
+                             ',height=' + height +
+                             ',top='    + top    +
+                             ',left='   + left;
+
+                    window.open(event.currentTarget.attributes[2].nodeValue, 'twitter', opts);
+
+                return false;
+            });
         }
     });
 
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,4 +9,26 @@ module ApplicationHelper
       "#{base_title} | #{page_title}"
     end
   end
+  
+  def get_meta(string, type)
+    if(string.empty?)
+      if(type == "description")
+        return "A very nice Ferrari website"
+      elsif(type == "title")
+        return "SCUDERIA.COM"
+      else
+        return "some image"
+      end
+    else
+      return string
+    end
+  end
+  
+  def get_meta_title(title, year)
+    if(title.empty?)
+      return "SCUDERIA.COM"
+    else
+      return "#{year} | #{title}"
+    end
+  end
 end
