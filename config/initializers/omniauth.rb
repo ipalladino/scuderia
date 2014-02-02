7c11de9eda94fd90f0b051bfc3cc181d656680a9
Ignacio Palladino <ignacio@flingo.tv>
2014-02-02 00:30:19 -0800

    adding proxy

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,6 @@
 OmniAuth.config.logger = Rails.logger
 
+
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_SECRET'], {:client_options => { :proxy => ENV["PROXIMO_URL"] }}
+  provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_SECRET'], {:client_options => { :proxy => ENV["PROXIMO_URL"] if ENV["PROXIMO_URL"] }}
 end
