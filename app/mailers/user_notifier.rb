8aaff5cfe91c032842859235978665cfa6d004a7
Ignacio Palladino(Raiden) <ipalladino@gmail.com>
2014-09-08 00:33:02 -0700

    Fixed itgit status!

--- app/mailers/user_notifier.rb
+++ app/mailers/user_notifier.rb
@@ -1,5 +1,5 @@
 class UserNotifier < ActionMailer::Base
-  default from: "Ignacio Palladino @scuderia"
+  default from: "Ignacio <no-reply@scuderia.com>"
 
   # send a signup email to the user, pass in the user object that   contains the user's email address
   def send_signup_email(user)
