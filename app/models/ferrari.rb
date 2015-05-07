06b7da972a7322118248ce12e031cccc0b26cf5e
Ignacio Palladino <ipalladino@splunk.com>
2015-05-06 18:25:56 -0700

    added rescue for missing publish date

--- app/models/ferrari.rb
+++ app/models/ferrari.rb
@@ -31,18 +31,21 @@ class Ferrari < ActiveRecord::Base
 
   def expired?
     if(self.published)
-      order_set = false
+
       begin
         publish_type = self.order.publish_setting
-        order_set = true
       rescue
-        puts "Ferrari Model, line 35 - no order setting - we will do nothing"
-        order_set = false
-      end
-      if(!order_set)
+        puts "Ferrari Model, expired? - no order setting - we will do nothing"
         return false
       end
-      days_since_creation = (Time.zone.now - self.publish_date) / 3600 / 24
+
+      begin
+        days_since_creation = (Time.zone.now - self.publish_date) / 3600 / 24
+      rescue
+        puts "Ferrari Model, expired? - There seems to be no publish date - we will do nothing"
+        return false
+      end
+
       if(publish_type == 0)
         #if publish setting is 0 expires in 1 year
         if(days_since_creation > 365)
--- lib/tasks/scheduler.rake
+++ lib/tasks/scheduler.rake
@@ -2,7 +2,9 @@ desc "This task is called by the Heroku scheduler add-on"
 task :expire_ferraris => :environment do
   puts "Expiring ferraris"
   Ferrari.all.each do |ferrari|
-    ferrari.expired?
+    puts "About to expire Ferrari:#{ferrari.id}"
+    expired = ferrari.expired?
+    puts "Ferrari expired #{expired}"
   end
   puts "done."
 end
